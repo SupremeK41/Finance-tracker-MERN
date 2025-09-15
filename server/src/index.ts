f663f43765ba9de4f29c827d56cc74295ad70ace
Karam-e-Abbas Ilyas <karamabbas110@gmail.com>
2025-09-15 18:32:06 +0100

    Finished Creating Base Application with backend setup and css

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
 //LLiPTFNdueFS6Sje
-import { error } from "console";
 import express, {Express} from "express";
 import mongoose from "mongoose";
-
+import finacialRecordRouter from './routes/financial-records';
+import cors from "cors";
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
+
 
 const mongoURI: string = 
 "mongodb+srv://karamabbas:LLiPTFNdueFS6Sje@personalfinancetracker.sf3wseo.mongodb.net/"
@@ -14,6 +16,8 @@ const mongoURI: string =
 mongoose.connect(mongoURI)
 .then(()=> console.log("CONNECTED TO MONGODB!"))
 .catch((err)=> console.error("failed to connect to MongoDB"))
+
+app.use("/financial-records", finacialRecordRouter);
 
 app.listen(port, () => {
   console.log(`Server running on ${port}`)
